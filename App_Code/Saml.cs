8ae07681030f9e214438d2bd1163c25480309560
Neil McGuire <neil.mcguire@gmail.com>
2016-08-03 09:14:20 -0400

    NotBefore/NotOnOrAfter Conditions check
    Ref #8. There are still quite a few conditions that are not checked in
    here, which could be present based on SAML Core
    (http://docs.oasis-open.org/security/saml/v2.0/saml-core-2.0-os.pdf),
    but let's make progress as it is needed.

--- App_Code/Saml.cs
+++ App_Code/Saml.cs
@@ -77,15 +77,46 @@ namespace OneLogin
 
             public bool IsValid()
             {
-                bool status = false;
-
+                bool status = true;
+                
                 XmlNamespaceManager manager = new XmlNamespaceManager(xmlDoc.NameTable);
                 manager.AddNamespace("ds", SignedXml.XmlDsigNamespaceUrl);
+                manager.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+                manager.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
                 XmlNodeList nodeList = xmlDoc.SelectNodes("//ds:Signature", manager);
 
                 SignedXml signedXml = new SignedXml(xmlDoc);
                 signedXml.LoadXml((XmlElement)nodeList[0]);
-                return signedXml.CheckSignature(certificate.cert, true);
+
+                status &= signedXml.CheckSignature(certificate.cert, true);
+
+                var notBefore = NotBefore();
+                status &= !notBefore.HasValue || (notBefore <= DateTime.Now);
+
+                var notOnOrAfter = NotOnOrAfter();
+                status &= !notOnOrAfter.HasValue || (notOnOrAfter > DateTime.Now);
+
+                return status;
+            }
+
+            public DateTime? NotBefore() {
+                XmlNamespaceManager manager = new XmlNamespaceManager(xmlDoc.NameTable);
+                manager.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+                manager.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
+
+                var nodes = xmlDoc.SelectNodes("/samlp:Response/saml:Assertion/saml:Conditions", manager);
+                var value = nodes?.Item(0)?.Attributes?["NotBefore"]?.Value;
+                return value != null ? DateTime.Parse(value) : (DateTime?)null;
+            }
+
+            public DateTime? NotOnOrAfter() {
+                XmlNamespaceManager manager = new XmlNamespaceManager(xmlDoc.NameTable);
+                manager.AddNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+                manager.AddNamespace("samlp", "urn:oasis:names:tc:SAML:2.0:protocol");
+
+                var nodes = xmlDoc.SelectNodes("/samlp:Response/saml:Assertion/saml:Conditions", manager);
+                var value = nodes?.Item(0)?.Attributes?["NotOnOrAfter"]?.Value;
+                return value != null ? DateTime.Parse(value) : (DateTime?)null;
             }
 
             public string GetNameID()
